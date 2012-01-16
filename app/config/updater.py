5070dba4902083d08f95d30c8080f86d27b313d2
kadeo <kadeo@yopmail.com>
2012-01-16 22:14:08 +0100

    changed to kadeo git

--- app/config/updater.py
+++ app/config/updater.py
@@ -17,9 +17,9 @@ log = CPLog(__name__)
 
 class Updater(rss, SimplePlugin):
 
-    git = 'git://github.com/RuudBurger/CouchPotato.git'
-    url = 'https://github.com/RuudBurger/CouchPotato/tarball/master'
-    downloads = 'https://github.com/RuudBurger/CouchPotato/downloads'
+    git = 'git://github.com/kadeo/CouchPotato.git'
+    url = 'https://github.com/kadeo/CouchPotato/tarball/master'
+    downloads = 'https://github.com/kadeo/CouchPotato/downloads'
     timeout = 10
     running = False
     version = None
